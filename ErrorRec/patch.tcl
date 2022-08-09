a15f774ba813a2992d2057f97fbbd5161590dc5f
YuCheng-Wu <b08901027@ntu.edu.tw>
2022-08-09 13:18:43 +0800

    Add files via upload

---
+++ ErrorRec/patch.tcl
@@ -0,0 +1,14 @@
+yosys -import
+
+set file [open "cec_blif/model.txt" r]
+set file_data [read $file]
+close $file
+set data [split $file_data "\n"]
+foreach line $data {
+     read_blif $line
+}
+
+synth -flatten
+techmap; opt
+#abc -g AND,NAND,OR,NOR,XOR,XNOR
+abc -g all
